77040e7aafb91e8d9a068af86180136489b3caa4
Nbiba Bedis <bedisnbiba@gmail.com>
2024-03-11 18:15:11 +0100

    more info

--- mod.ts
+++ mod.ts
@@ -7,8 +7,24 @@ It uses `deno doc --json` to get the needed informations
 
 ## Usage
 
+By default its outputs to stdout
+
+```
+deno run -A jsr:@sigmasd/doc-gen entrypoint.ts
+```
+
+If you want to create/update README.md you can use shell redirection
+
 ```
 deno run -A jsr:@sigmasd/doc-gen entrypoint.ts > README.md
+```
+
+Or add a convenience task in deno.json
+
+```json
+"tasks": {
+  "doc": "deno run --allow-run=deno jsr:@sigmasd/doc-gen ./entrypoint.ts > README.md"
+}
 ```
 
 @module
